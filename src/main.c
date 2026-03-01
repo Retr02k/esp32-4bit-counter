00ab13e7ecb708f6cd56f981ed6e60d51152fb56
Retr02k <pedro210332@gmail.com>
2026-03-01 22:01:19 +0000

    Refactor LED counter and button logic, improve scalability
    - Replaced individual LED #defines with a single LED array and LED_COUNT
    - Simplified GPIO setup using a loop instead of repeated calls
    - Fixed LED toggle logic: correctly extract each bit from the counter using (counter >> i) & 1
    - Removed redundant set_initial_led_value() function and use toggle_led(counter) at startup
    - Counter now wraps automatically using modulo with (1 << LED_COUNT) for scalability
    - Minor code cleanup: removed unnecessary BIT_MASK macro
    
    Pending / TODO:
    - Implement proper button debounce to avoid multiple triggers per press
    - Optionally refine printf debugging for cleaner runtime feedback

--- src/main.c
+++ src/main.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 #define	BUTTON		4
-#define FIRST_LED	5
-#define SECOND_LED	18
-#define THIRD_LED	19
-#define FOURTH_LED	21
+#define LED_COUNT	4
+
+const int led[] = {5, 18, 19, 21};
 
 int	button_press(int state) {
 	static	int last_state = 0;
 	int	ret = 0;
 	if (!last_state && state){
 	ret = 1;
-	printf("Butoon pressed!\n");
 	}
 
 	last_state = state;
@@ -22,9 +21,10 @@ int	button_press(int state) {
 }
 
 void	toggle_led(unsigned int counter){
-	int	led[] = {FIRST_LED, SECOND_LED, THIRD_LED, FOURTH_LED};
-	for (int i = 0; i < 4; i++){
-		gpio_set_level(led[i], counter & (1 << i));
+	for (int i = 0; i < LED_COUNT; i++){
+		int state = ((counter >> i) & 1);
+		
+		gpio_set_level(led[i], state);
 	}
 }
 
@@ -34,20 +34,23 @@ void app_main() {
 
 	gpio_set_direction(BUTTON, GPIO_MODE_INPUT);
 	gpio_set_pull_mode(BUTTON, GPIO_PULLDOWN_ONLY);
-	gpio_set_direction(FIRST_LED, GPIO_MODE_OUTPUT);
-	gpio_set_direction(SECOND_LED, GPIO_MODE_OUTPUT);
-	gpio_set_direction(THIRD_LED, GPIO_MODE_OUTPUT);
-	gpio_set_direction(FOURTH_LED, GPIO_MODE_OUTPUT);
+
+	for(int i = 0; i < LED_COUNT; i++){
+		gpio_set_direction(led[i], GPIO_MODE_OUTPUT);
+	}
+
+	toggle_led(counter);
 
 	while (1)
 	{
 		int button_state = gpio_get_level(BUTTON);
 		
-		// if (button_press(button_state)){
-		counter = (counter + 1) % 16;
-		toggle_led(counter);
-		// }
+		if (button_press(button_state)){
+			counter = (counter + 1) % (1 << LED_COUNT);
+			printf("Button pressed!");
+			toggle_led(counter);
+		}
 		
-		vTaskDelay(pdMS_TO_TICKS(1000));
+		vTaskDelay(pdMS_TO_TICKS(10));
 	}
 }
